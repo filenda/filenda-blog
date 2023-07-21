5f794971191fd2e3992875021feae5cbbeda0427
Vinicius Filenga <vinifilenga@hotmail.com>
2023-07-20 23:32:59 -0300

    feat: resume upload and added link on top nav

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/tags', title: 'Tags' },
   // { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: '/vinifilenga-resume.pdf', title: 'Resume' },
 ]
 
 export default headerNavLinks
